07d6271c8bdc00f392737d13fef492c247398dfd
parveen kaushik <parveen.ase@antiersolutions.com>
2025-05-12 18:07:43 +0530

    feat: cosmvisor script for testnet

--- shido_ubuntu_node.sh
+++ shido_ubuntu_node.sh
@@ -7,9 +7,11 @@
 #fi
 current_path=$(pwd)
 bash  $current_path/install-go.sh 
-bash install-go.sh
-source ~/.bashrc
+
+source $HOME/.bashrc
 ulimit -n 16384
+
+go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.5.0
 
 # Get OS and version
 OS=$(awk -F '=' '/^NAME/{print $2}' /etc/os-release | awk '{print $1}' | tr -d '"')
@@ -42,18 +44,16 @@ else
 fi
 
 sudo cp $current_path/libwasmvm.x86_64.so /usr/lib
-wget https://testnet-blockchain-shido.s3.us-west-2.amazonaws.com/snapshot15apr2024.zip
 #==========================================================================================================================================
-KEYS="alice"
+KEYS="katty"
 CHAINID="shido_9007-1"
-MONIKER="CosmoGalaxy"
-KEYRING="test"
+KEYRING="os"
+MONIKER="shidoValidator"
 KEYALGO="eth_secp256k1"
 LOGLEVEL="info"
 
 # Set dedicated home directory for the shidod instance
-# HOMEDIR="$HOME/.tmp-shidod"
-HOMEDIR="$HOME/.tmp-shidod"
+ HOMEDIR="/data/.tmp-shidod"
 
 # Path variables
 CONFIG=$HOMEDIR/config/config.toml
@@ -83,11 +83,11 @@ fi
 # Setup local node if overwrite is set to Yes, otherwise skip setup
 if [[ $overwrite == "y" || $overwrite == "Y" ]]; then
 	# Remove the previous folder
-	file_path="/etc/systemd/system/shido.service"
+	file_path="/etc/systemd/system/shidochain.service"
 
 # Check if the file exists
 if [ -e "$file_path" ]; then
-sudo systemctl stop shido.service
+sudo systemctl stop shidochain.service
     echo "The file $file_path exists."
 fi
 	sudo rm -rf "$HOMEDIR"
@@ -95,7 +95,7 @@ fi
 	# Set client config
 	shidod config keyring-backend $KEYRING --home "$HOMEDIR"
 	shidod config chain-id $CHAINID --home "$HOMEDIR"
-    echo "===========================Copy these keys with mnemonics aand save it in safe place ==================================="
+    echo "===========================Copy these keys with mnemonics and save it in safe place ==================================="
 	shidod keys add $KEYS --keyring-backend $KEYRING --algo $KEYALGO --home "$HOMEDIR"
 	echo "========================================================================================================================"
 	echo "========================================================================================================================"
@@ -103,79 +103,89 @@ fi
 
 	#changes status in app,config files
     sed -i 's/timeout_commit = "3s"/timeout_commit = "1s"/g' "$CONFIG"
+    sed -i 's/pruning = "default"/pruning = "custom"/g' "$APP_TOML"
+    sed -i 's/pruning-keep-recent = "0"/pruning-keep-recent = "100000"/g' "$APP_TOML"
+    sed -i 's/pruning-interval = "0"/pruning-interval = "100"/g' "$APP_TOML"
     sed -i 's/seeds = ""/seeds = ""/g' "$CONFIG"
     sed -i 's/prometheus = false/prometheus = true/' "$CONFIG"
-	sed -i 's/experimental_websocket_write_buffer_size = 200/experimental_websocket_write_buffer_size = 600/' "$CONFIG"
+    sed -i 's/experimental_websocket_write_buffer_size = 200/experimental_websocket_write_buffer_size = 600/' "$CONFIG"
     sed -i 's/prometheus-retention-time  = "0"/prometheus-retention-time  = "1000000000000"/g' "$APP_TOML"
     sed -i 's/enabled = false/enabled = true/g' "$APP_TOML"
+    sed -i 's/minimum-gas-prices = "0shido"/minimum-gas-prices = "0.25shido"/g' "$APP_TOML"
     sed -i 's/enable = false/enable = true/g' "$APP_TOML"
     sed -i 's/swagger = false/swagger = true/g' "$APP_TOML"
-	sed -i 's/enabled-unsafe-cors = false/enabled-unsafe-cors = true/g' "$APP_TOML"
+    sed -i 's/enabled-unsafe-cors = false/enabled-unsafe-cors = true/g' "$APP_TOML"
+    sed -i 's/enable-unsafe-cors = false/enable-unsafe-cors = true/g' "$APP_TOML"
+        sed -i '/\[rosetta\]/,/^\[.*\]/ s/enable = true/enable = false/' "$APP_TOML"
 	sed -i 's/localhost/0.0.0.0/g' "$APP_TOML"
     sed -i 's/localhost/0.0.0.0/g' "$CONFIG"
+    sed -i 's/:26660/0.0.0.0:26660/g' "$CONFIG"
     sed -i 's/localhost/0.0.0.0/g' "$CLIENT"
     sed -i 's/127.0.0.1/0.0.0.0/g' "$APP_TOML"
     sed -i 's/127.0.0.1/0.0.0.0/g' "$CONFIG"
     sed -i 's/127.0.0.1/0.0.0.0/g' "$CLIENT"
-	sed -i 's/\[\]/["*"]/g' "$CONFIG"
+    sed -i 's/\[\]/["*"]/g' "$CONFIG"
 	sed -i 's/\["\*",\]/["*"]/g' "$CONFIG"
-    sed -i 's/127.0.0.1/0.0.0.0/g' "$CLIENT"
-
+  
+  # sed -i 's/enable = false/enable = true/g' "$CONFIG"
+	 
 
 	# these are some of the node ids help to sync the node with p2p connections
-	sed -i 's/persistent_peers \s*=\s* ""/persistent_peers = "4977b90f62a70c089ae1ad9c7640c2f511089b94@100.21.69.117:26656,1b44413d629ac7c8d30bf8010792962d22106451@44.236.180.179:26656,639c2185f664fd26462bd5c8a7d5140ba1705211@44.227.80.206:26656"/g' "$CONFIG"
+	 sed -i 's/persistent_peers \s*=\s* ""/persistent_peers = "7248e4bc6936f39090e4b9a0b50122abd5adc965@35.82.44.23:26656,518d20eff4c02cfd9a7a31b06df4f89f813594e0@100.21.69.117:25556,2333b40fe2a6c290cd187bc8e6ea6dc19ec0e9b6@44.236.180.179:26656,7bd130ba4991664d19fc7cfeb00d6a3cbcc8eed0@44.227.80.206:26656"/g' "$CONFIG"
 
 	# remove the genesis file from binary
-	rm -rf $HOMEDIR/config/genesis.json
+	 rm -rf $HOMEDIR/config/genesis.json
 
-	# paste the genesis file
-	cp $current_path/genesis.json $HOMEDIR/config
+	# # paste the genesis file
+	 cp $current_path/genesis.json $HOMEDIR/config
 
 	# Run this to ensure everything worked and that the genesis file is setup correctly
 	shidod validate-genesis --home "$HOMEDIR"
 
- 	cd $HOMEDIR/data
-	unzip $current_path/snapshot15apr2024.zip
+	echo "export DAEMON_NAME=shidod" >> ~/.profile
+    echo "export DAEMON_HOME="$HOMEDIR"" >> ~/.profile
+    source ~/.profile
+    echo $DAEMON_HOME
+    echo $DAEMON_NAME
 
-	ADDRESS=$(shidod keys list --home $HOMEDIR --keyring-backend $KEYRING | grep "address" | cut -c12-)
-	WALLETADDRESS=$(shidod debug addr $ADDRESS --home $HOMEDIR --keyring-backend $KEYRING)
-	# TENDERMINTPUBKEY=$(shidod tendermint show-validator --home $HOMEDIR | grep "key" | cut -c12-)
-	# NodeId=$(shidod tendermint show-node-id --home $HOMEDIR --keyring-backend $KEYRING)
-	# BECH32ADDRESS=$(shidod keys show ${KEYS} --home $HOMEDIR --keyring-backend $KEYRING| grep "address" | cut -c12-)
-	# BECH32ADDRESS12=$(shidod keys show ${KEYS} --home $HOMEDIR --keyring-backend $KEYRING)
-	# PRIVATEKEY=$(shidod keys export ${KEYS} --unarmored-hex --unsafe --home $HOMEDIR --keyring-backend $KEYRING)
+	cosmovisor init "${INSTALL_PATH}${BINARY}"
+
+	
+	TENDERMINTPUBKEY=$(shidod tendermint show-validator --home $HOMEDIR | grep "key" | cut -c12-)
+	NodeId=$(shidod tendermint show-node-id --home $HOMEDIR --keyring-backend $KEYRING)
+	BECH32ADDRESS=$(shidod keys show ${KEYS} --home $HOMEDIR --keyring-backend $KEYRING| grep "address" | cut -c12-)
 
 	echo "========================================================================================================================"
-	echo "Shido Eth Hex Address==== "$WALLETADDRESS
-	# echo "tendermint Key==== "$TENDERMINTPUBKEY
-	# echo "BECH32Address==== "$BECH32ADDRESS
-	# echo "Bech32addres123s ===" $BECH32ADDRESS12
-	# echo "NodeId ===" $NodeId
-	# echo "PRIVATEKEY ===" $PRIVATEKEY
+	echo "tendermint Key==== "$TENDERMINTPUBKEY
+	echo "BECH32Address==== "$BECH32ADDRESS
+	echo "NodeId ===" $NodeId
 	echo "========================================================================================================================"
 
 fi
 
 #========================================================================================================================================================
-# Start the node
-# shidod start --home "$HOMEDIR"
 sudo su -c  "echo '[Unit]
-Description=shidod Service
-After=network.target
-
+Description=Shido Node
+Wants=network-online.target
+After=network-online.target
 [Service]
-Type=simple
 User=$(whoami)
 Group=$(whoami)
-ExecStart=/usr/local/bin/shidod start --home $HOMEDIR
+Type=simple
+ExecStart=/home/$(whoami)/go/bin/cosmovisor run start --home $DAEMON_HOME
 Restart=always
-
-Environment=HOME=$HOMEDIR
-
+RestartSec=3
+LimitNOFILE=4096
+Environment="DAEMON_NAME=shidod"
+Environment="DAEMON_HOME="$HOMEDIR""
+Environment="DAEMON_ALLOW_DOWNLOAD_BINARIES=false"
+Environment="DAEMON_RESTART_AFTER_UPGRADE=true"
+Environment="DAEMON_LOG_BUFFER_SIZE=512"
+Environment="UNSAFE_SKIP_BACKUP=false"
 [Install]
-WantedBy=multi-user.target'> /etc/systemd/system/shido.service"
-
+WantedBy=multi-user.target'> /etc/systemd/system/shidochain.service"
 
 sudo systemctl daemon-reload
-sudo systemctl enable shido.service
-sudo systemctl start shido.service
+sudo systemctl enable shidochain.service
+
+sudo systemctl start shidochain.service
